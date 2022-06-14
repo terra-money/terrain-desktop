40cc30707142ed4cb04c48cb094393b46af36914
plubber <er.h.gorski@gmail.com>
2022-06-14 17:53:17 -0400

    fix: styling and linking

--- src/component/TransactionView.tsx
+++ src/component/TransactionView.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { FINDER_URL } from '../constants';
+import { truncate } from '../utils';
 
 function TransactionItemView({ tx }: { tx: any }) {
   const txHref = `${FINDER_URL}/tx/${tx.txhash}`;
 
   return (
-    <a target="_blank" href={txHref} className="w-full text-right flex" rel="noreferrer">
-      <div className="w-1/12 bg-blue-200 px-2 text-center py-2">
-        <p className="text-xs text-blue-800 font-bold">Hash</p>
-        <p className="text-blue-800">{tx.txhash.replace(/(.{7})..+/, '$1…')}</p>
-      </div>
+    <div className="w-full text-right flex">
+      <a target="_blank" href={txHref} rel="noreferrer">
+        <div className="w-1/12 bg-blue-200 px-2 text-center py-2">
+          <p className="text-xs text-blue-800 font-bold">Hash</p>
+          <p className="text-blue-800">{truncate(tx.txhash)}</p>
+        </div>
+      </a>
       <div className="w-11/12 flex justify-between px-4">
         <div>
           <p>Block Height</p>
@@ -19,9 +22,9 @@ function TransactionItemView({ tx }: { tx: any }) {
           <p>Gas Used</p>
           <p>{tx.result.gas_used}</p>
         </div>
-
       </div>
-    </a>
+
+    </div>
   );
 }
 
