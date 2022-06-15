0ec44276f4cee0b972161c7230ece323428914f8
plubber <er.h.gorski@gmail.com>
2022-06-15 17:44:59 -0400

    feat: validate local terra dir path

--- utils.js
+++ utils.js
@@ -2,7 +2,12 @@ const settings = require('electron-settings');
 const { dialog, app } = require('electron');
 const { spawn } = require('child_process');
 const { WebSocketClient } = require('@terra-money/terra.js');
-const { LOCALTERRA_PATH_DIALOG, LOCALTERRA_STOP_DIALOG, LOCAL_WS } = require('./constants');
+const { promises: fs } = require('fs');
+const yaml = require('js-yaml');
+
+const {
+  LOCALTERRA_PATH_DIALOG, LOCALTERRA_STOP_DIALOG, LOCAL_WS, LOCALTERRA_BAD_DIR_DIALOG,
+} = require('./constants');
 
 let compose;
 const isExiting = false;
@@ -11,13 +16,29 @@ let isStarted = false;
 const blockWs = new WebSocketClient(LOCAL_WS);
 const txWs = new WebSocketClient(LOCAL_WS);
 
+async function validatePath(path) {
+  const ltFile = await fs.readFile(`${path}/docker-compose.yml`, 'utf8');
+  const { services, version } = yaml.load(ltFile); // we also have easy access to version here
+  const ltServices = Object.keys(services); // could handle this in a bunch of diff ways
+  return ltServices.includes('terrad');
+}
+
 async function getLocalTerraPath() {
-  let ltPath = await settings.get('localTerraPath');
-  if (!ltPath) {
-    const { filePaths } = await dialog.showOpenDialog(LOCALTERRA_PATH_DIALOG);
-    ltPath = await settings.set('localTerraPath', filePaths[0]);
+  try {
+    let ltPath = await settings.get('localTerraPath');
+    if (!ltPath) {
+      const { filePaths } = await dialog.showOpenDialog(LOCALTERRA_PATH_DIALOG);
+      const isValid = await validatePath(filePaths[0]);
+      if (isValid) {
+        [ltPath] = filePaths;
+        settings.set('localTerraPath', ltPath);
+      }
+    }
+    return ltPath;
+  } catch (err) {
+    await dialog.showMessageBox(LOCALTERRA_BAD_DIR_DIALOG);
+    startLocalTerra();
   }
-  return ltPath;
 }
 
 async function startLocalTerra() {
@@ -41,9 +62,9 @@ async function startLocalTerra() {
     if (isExiting) {
       app.quit();
     } else {
-      dialog.showMessageBoxSync(LOCALTERRA_STOP_DIALOG);
+      // dialog.showMessageBoxSync(LOCALTERRA_STOP_DIALOG);
       isStarted = false;
-      startLocalTerra();
+      // startLocalTerra();
     }
   });
   return compose;
