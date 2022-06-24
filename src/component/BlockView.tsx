3da0e05dee3432a1bfae44e8ebceed99db4e1470
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2022-06-24 14:10:41 -0700

    style: updating text positioning

--- src/component/BlockView.tsx
+++ src/component/BlockView.tsx
@@ -11,22 +11,22 @@ function BlockItemView({ block }: { block: BlockInfo }) {
   const blockHref = `${process.env.REACT_APP_FINDER_URL}/blocks/${height}`;
 
   return (
-    <a href={blockHref} target="_blank" className="shadow-sm w-full text-right flex" rel="noreferrer">
-      <div className="w-2/12 bg-blue-200/25 p-2 text-center">
+    <a href={blockHref} target="_blank" className="shadow-sm w-full items-center text-center flex" rel="noreferrer">
+      <div className="w-2/12 bg-blue-200/25 p-4">
         <p className="text-xs text-blue-800 font-bold">Block Number</p>
         <p className="text-blue-800">{height}</p>
       </div>
-      <div className="flex w-10/12 text-center justify-around py-3">
+      <div className="flex w-10/12 justify-around">
         <div>
-          <p className="rounded-full bg-terra-dark-blue/75 text-white px-2">Mined</p>
+          <p className="rounded-full bg-terra-dark-blue/75 text-white">Mined</p>
           <p>{dateString}</p>
         </div>
         <div>
-          <p className="rounded-full bg-terra-dark-blue/75 text-white px-2">Gas Used</p>
+          <p className="rounded-full bg-terra-dark-blue/75 text-white px-4">Gas Used</p>
           <p>{gasUsed}</p>
         </div>
         <div>
-          <p className="rounded-full bg-terra-dark-blue/75 text-white px-2">Transactions</p>
+          <p className="rounded-full bg-terra-dark-blue/75 text-white px-4">Transactions</p>
           <p>{txInfos.length}</p>
         </div>
       </div>
