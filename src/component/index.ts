21ea94f7e085eea4713bff77eb58516084c7827e
plubber <er.h.gorski@gmail.com>
2022-07-11 15:08:26 -0400

    feat: fix timestamp and naming cleanup

--- src/component/index.ts
+++ src/component/index.ts
@@ -2,4 +2,5 @@ export { default as AccountView } from './AccountView';
 export { default as KeyView } from './KeyView';
 export { default as LogView } from './LogView';
 export { default as ContractView } from './ContractView';
+export { default as BlockView } from './BlockView';
 export { default as NavLink } from './NavLink';
