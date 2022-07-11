f3b52ef6eb29594f67fdb3af56226d47e054d3ba
plubber <er.h.gorski@gmail.com>
2022-07-11 12:14:58 -0400

    rem: events page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,4 @@ export { default as LogsPage } from './Logs';
 export { default as TransactionsPage } from './Transactions';
 export { default as BlockPage } from './Block';
 export { default as ContractsPage } from './Contracts';
-export { default as EventsPage } from './Events';
 export { default as OnboardPage } from './Onboard';
