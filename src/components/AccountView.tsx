28662d730cb1e31da967ef44b712d07e92bc97d0
plubber <er.h.gorski@gmail.com>
2022-10-11 10:55:59 -0500

    chore: refactor svg

--- src/components/AccountView.tsx
+++ src/components/AccountView.tsx
@@ -5,6 +5,7 @@ import { useTerraBlockUpdate, useLocalTerraStarted } from '../hooks/terra';
 import { demicrofy, nFormatter } from '../utils';
 import { REACT_APP_FINDER_URL } from '../constants';
 import { KeyViewModal } from '.';
+import { ReactComponent as KeyIcon } from '../assets/icons/menu/key.svg';
 
 function AccountView({
   wallet,
@@ -64,20 +65,7 @@ function AccountView({
           )}
           className="flex justify-end text-blue-700 hover:text-blue-500 px-5 pr-7"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
-            />
-          </svg>
+          <KeyIcon className="h-6 w-6" />
         </button>
       </div>
     </div>
