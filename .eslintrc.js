e5d7b8e9a4631820bc47c869af7a85790223e3d0
plubber <er.h.gorski@gmail.com>
2022-10-19 16:21:13 -0500

    wip: test actions 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  ignorePatterns: ['node_modules/**/*', 'extensions/**/*'],
   plugins: [
     '@typescript-eslint',
   ],
