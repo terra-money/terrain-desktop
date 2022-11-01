5f5c4de9f73d912914b93cc764db9706178ff2c8
plubber <er.h.gorski@gmail.com>
2022-11-01 11:22:45 -0500

    wip: app signing

--- create-dmgs.js
+++ create-dmgs.js
@@ -1,17 +1,28 @@
 const createDMG = require('electron-installer-dmg');
 const fs = require('fs');
+const { signAsync } = require('@electron/osx-sign');
 
-fs.readdirSync('./out').forEach((file) => {
+fs.readdirSync('./out').forEach(async (file) => {
   if (file === 'make') return;
-  console.log('Creating DMG for: ', file);
-  createDMG({
-    appPath: `./out/${file}/Terrarium.app`,
-    out: 'installers',
-    name: file,
-    overwrite: true,
-    debug: true,
-    icon: './src/assets/icons/icon.icns',
-  }, (err) => {
+  const appPath = `./out/${file}/Terrarium.app`;
+
+  try {
+    console.log('Signing:', file);
+    await signAsync({
+      app: appPath,
+      identity: 'Developer ID Application: Jason Stallings (75EGRT7282)',
+    });
+
+    console.log('Creating DMG for: ', file);
+    await createDMG({
+      appPath,
+      out: 'installers',
+      name: file,
+      overwrite: true,
+      debug: true,
+      icon: './src/assets/icons/icon.icns',
+    });
+  } catch (err) {
     console.log(`Error building DMG ${file}: `, err);
-  });
+  }
 });
